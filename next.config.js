5e30400308c985b26be9aabce3726b726e8368cb
Gustavo Castro <105456696+GustavoCastrohhh@users.noreply.github.com>
2025-10-23 10:38:53 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,15 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  async redirects() {
+    return [
+      {
+        source: '/', // Rota de origem (a raiz do site)
+        destination: '/login', // Rota de destino (a página de login)
+        permanent: true, // Define se o redirecionamento é permanente (melhor para SEO)
+      },
+    ]
+  },
 };
 
 module.exports = nextConfig;
