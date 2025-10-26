f2529a99539a84671e926a6b5bde6c38575b3f36
Gustavo Castro <gustavo.castrohhh@gmail.com>
2025-10-25 21:02:44 -0300

    alteração nome dashboard

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { Toaster } from '@/components/ui/sonner';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Dashboard de Disparo',
+  title: 'Cristais Conect',
   description: 'Plataforma de disparo de mensagens',
 };
 
